21a0c36d941649a49818bcf40bbf1d8e9038077a
mikenobbs <45309438+mikenobbs@users.noreply.github.com>
2024-04-12 05:10:33 +0100

    [1] fixed backup copy bug, collection posters now work

--- assetassistant.py
+++ assetassistant.py
@@ -3,6 +3,30 @@ import shutil
 import yaml
 import time
 import requests
+from datetime import datetime
+
+
+# Parse YAML config file
+with open('config.yml', 'r') as f:
+    config = yaml.safe_load(f)
+
+# Load directory paths from config
+process_dir = config['process']
+shows_dir = config['shows']
+movies_dir = config['movies']
+collections_dir = config['collections']
+script_dir = os.path.dirname(os.path.abspath(__file__))
+failed_dir = os.path.join(script_dir, 'failed')
+backup_enabled = config.get('enable_backup', False)
+backup_dir = os.path.join(script_dir, 'backup')
+
+# Create failed directory if it doesn't exist
+if not os.path.exists(failed_dir):
+    os.makedirs(failed_dir)
+
+# Create backup directory if backup is enabled and it doesn't exist
+if backup_enabled and not os.path.exists(backup_dir):
+    os.makedirs(backup_dir)
 
 # Match filename with directory names and copy the file
 def match_and_copy(filename, process_dir, shows_dir, movies_dir, collections_dir, failed_dir, copied_files):
@@ -25,13 +49,24 @@ def match_and_copy(filename, process_dir, shows_dir, movies_dir, collections_dir
     else:
         # If filename doesn't contain parentheses, search in collections directory
         for dir_name in os.listdir(collections_dir):
-            if name in dir_name:
-                # Copy the file to the matching directory
-                src = os.path.join(process_dir, filename)
-                dest = os.path.join(collections_dir, dir_name, filename)
-                shutil.copy(src, dest)
-                copied_files.append(dest)
-                return collections_dir
+            if "Collection" in name:
+                name_without_collection = name.replace("Collection", "").strip()
+                if name_without_collection in dir_name:
+                    # Copy the file to the matching directory
+                    src = os.path.join(process_dir, filename)
+                    dest = os.path.join(collections_dir, dir_name, filename)
+                    shutil.copy(src, dest)
+                    copied_files.append(dest)
+                    return collections_dir
+            else:
+                if name in dir_name:
+                    # Copy the file to the matching directory
+                    src = os.path.join(process_dir, filename)
+                    dest = os.path.join(collections_dir, dir_name, filename)
+                    shutil.copy(src, dest)
+                    copied_files.append(dest)
+                    return collections_dir
+
     # If no matching directory is found, move to failed directory
     move_to_failed(filename, process_dir, failed_dir)
     return None
@@ -40,29 +75,7 @@ def match_and_copy(filename, process_dir, shows_dir, movies_dir, collections_dir
 def move_to_failed(filename, process_dir, failed_dir):
     src = os.path.join(process_dir, filename)
     dest = os.path.join(failed_dir, filename)
-    shutil.move(src, dest)
-
-# Parse YAML config file
-with open('config.yml', 'r') as f:
-    config = yaml.safe_load(f)
-
-# Load directory paths from config
-process_dir = config['process_dir']
-shows_dir = config['shows_dir']
-movies_dir = config['movies_dir']
-collections_dir = config['collections_dir']
-script_dir = os.path.dirname(os.path.abspath(__file__))
-failed_dir = os.path.join(script_dir, 'failed')
-backup_enabled = config.get('enable_backup', False)
-backup_dir = os.path.join(script_dir, 'backup')
-
-# Create failed directory if it doesn't exist
-if not os.path.exists(failed_dir):
-    os.makedirs(failed_dir)
-
-# Create backup directory if backup is enabled and it doesn't exist
-if backup_enabled and not os.path.exists(backup_dir):
-    os.makedirs(backup_dir)
+    shutil.copy(src, dest)
 
 # List to store successfully copied files
 copied_files = []
@@ -79,42 +92,11 @@ directory_names = {
     collections_dir: 'collections_dir',
     failed_dir: 'failed_dir'
 }
-
-# Process each file in the process_dir
+            
 for filename in os.listdir(process_dir):
     destination = match_and_copy(filename, process_dir, shows_dir, movies_dir, collections_dir, failed_dir, copied_files)
     if destination:
         moved_counts[directory_names[destination]] += 1
-    
-    # Check if the destination directory exists
-    if not os.path.exists(destination):
-        print(f"Error: Destination directory '{destination}' does not exist.")
-        continue
-    
-    # Define source and destination paths
-    src = os.path.join(process_dir, filename)
-    dest = os.path.join(destination, filename)
-    
-    # Copy the file to the destination directory
-    try:
-        shutil.copy(src, dest)
-        print(f"File '{filename}' copied to destination successfully.")
-    
-        # Check if the file exists in the destination directory before moving it to the backup directory
-        if os.path.exists(dest):
-            try:
-                shutil.move(dest, os.path.join(backup_dir, filename))
-                print(f"File '{filename}' moved to backup successfully.")
-            except Exception as e:
-                print(f"Error moving file to backup: {e}")
-        else:
-            print(f"Error: File '{filename}' does not exist in the destination directory.")
-        
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found. Skipping.")
-    except Exception as e:
-        print(f"Error: {e}")
-    
 
 # Record end time
 end_time = time.time()
@@ -122,27 +104,37 @@ end_time = time.time()
 # Calculate total runtime
 total_runtime = end_time - start_time
 
-# Move successfully copied files to backup directory if backup is enabled
+# Copy successfully copied files to backup directory if backup is enabled
 if backup_enabled:
     for file_path in copied_files:
-        src = os.path.join(process_dir, filename)
-        shutil.move(src, os.path.join(backup_dir, filename))
-  
+        shutil.copy(file_path, os.path.join(backup_dir, os.path.basename(file_path)))
+        
+# Remove all files from process_dir
+for filename in os.listdir(process_dir):
+    file_path = os.path.join(process_dir, filename)
+    os.remove(file_path)
 
 # Create a formatted summary
-summary = f"**Summary:**\n\n"
-summary += f"**TV Shows:**\n {moved_counts['shows_dir']}\n"
-summary += f"**Movies:**\n {moved_counts['movies_dir']}\n"
-summary += f"**Collections:**\n {moved_counts['collections_dir']}\n"
-summary += f"**Failures:**\n {moved_counts['failed_dir']}\n\n"
-summary += f"**Backup?**\n {'Yes' if backup_enabled else 'No'}\n\n"
-summary += f"**Total run time:**\n {total_runtime:.2f} seconds.\n"
+summary = f"**Movie Posters:**\n {moved_counts['movies_dir']}\n"
+summary += f"**TV Show Posters:**\n {moved_counts['shows_dir']}\n"
+summary += f"**Collection Posters:**\n {moved_counts['collections_dir']}\n"
+summary += f"**Failures:**\n {moved_counts['failed_dir']}\n"
+summary += f"**Backup Enabled?**\n {'Yes' if backup_enabled else 'No'}\n"
+summary += f"**Total Run Time:**\n {total_runtime:.2f} seconds\n"
 
+# Get the current date
+current_date = datetime.now().date()
+
+# Read the script version from version.txt
+version = os.path.join(os.path.dirname(__file__), 'VERSION')
+with open(version, 'r') as f:
+    script_version = f.read().strip()
+    
 # Send summary message to Discord webhook as an embed
 webhook_url = config.get('webhook_url')
-image_url = 
-footer_text = ""
-color = 0xFF0000 
+image_url = "https://raw.githubusercontent.com/mikenobbs/AssetAssistant/main/logo/icon.png"
+footer_text = f"AssetAssistant [v{script_version}] | {current_date.strftime('%d/%m/%Y %H:%M')}"
+color = 0x9E9E9E
 
 if webhook_url:
     # Construct the embed object with custom values
@@ -156,12 +148,3 @@ if webhook_url:
     
     # Send the embed data to the Discord webhook using a POST request
     response = requests.post(webhook_url, json={"embeds": [embed]})
-    
-    # Check if the request was successful
-    if response.status_code != 200:
-        print("Failed to send summary message to Discord.")
-else:
-    print("No webhook URL provided. Discord notification disabled.")
-
-# Print summary message
-print(summary)
