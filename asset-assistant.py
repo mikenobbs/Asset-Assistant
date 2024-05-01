fe0c73ea4dc32ae2e1b96d56cd97782b445e68fa
mikenobbs <45309438+mikenobbs@users.noreply.github.com>
2024-05-02 00:15:14 +0100

    renamed and added full logging

--- asset-assistant.py
+++ asset-assistant.py
@@ -1,19 +1,40 @@
 import os
-import shutil
-import yaml
-import time
-import requests
 import PIL.Image
+import platform
+import requests
+import shutil
+import sys
+import time
+import yaml
 from datetime import datetime
+from modules.logs import MyLogger
 
-# Parse YAML config file
-with open('config.yml', 'r') as f:
-    config = yaml.safe_load(f)
+logger = MyLogger()
 
-# Load directory paths from config
+## Start ##
+start_time = time.time()
+with open("VERSION", "r") as f:
+    version = f.read().strip()
+    logger.info(f"    Version: v{version}")
+platform_info = platform.platform()
+logger.info(f"    Platform: {platform.platform()}")
+logger.separator(text="Asset Assistent Starting", debug=False)
+  
+## Load config ##  
+try:
+    with open('config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+        logger.info(" Loading config.yml...")
+        logger.info(" Config loaded successfully")
+        logger.separator(text="Config", space=False, border=False, debug=True)
+except FileNotFoundError:
+    logger.error(f" Config file 'config.yml' not found at {os.path.dirname(os.path.abspath(__file__))}. Terminating script.")
+    sys.exit(1)
+
+## Paths ##
 process_dir = config['process']
-shows_dir = config['shows']
 movies_dir = config['movies']
+shows_dir = config['shows']
 collections_dir = config['collections']
 script_dir = os.path.dirname(os.path.abspath(__file__))
 failed_dir = os.path.join(script_dir, 'failed')
@@ -21,84 +42,125 @@ backup_enabled = config.get('enable_backup', False)
 backup_dir = os.path.join(script_dir, 'backup')
 naming_convention = config.get('naming_convention', None)
 
-# Create failed directory if it doesn't exist
+logger.debug(" Process directory:")
+logger.debug(f" - {process_dir}")
+logger.debug(f" Movies directory:")
+logger.debug(f" - {movies_dir}")
+logger.debug(f" TV Shows directory:")
+logger.debug(f" - {shows_dir}")
+logger.debug(f" Collections directory:")
+logger.debug(f" - {collections_dir}")
+logger.debug("")
+
+## Naming convention ##
+if naming_convention == None:
+    logger.warn(" Naming convention:") 
+    logger.debug("   Skipping:") 
+    logger.debug("   - Collection assets")
+    logger.debug("")
+else:
+    if naming_convention == "kodi" or naming_convention == "kometa":
+        logger.debug(f" Naming convention: {naming_convention.capitalize()}")
+        logger.debug("   Enabling:")
+        logger.debug("   - Collection assets")
+        logger.debug("")
+    else:
+        logger.debug(f" Naming convention: {naming_convention.capitalize()}")
+        logger.debug("   Skipping:")
+        logger.debug("   - Collection assets")
+        logger.debug("")
+
+## Failed Directory ##
 if not os.path.exists(failed_dir):
     os.makedirs(failed_dir)
+    logger.debug(" Failed directory not found...")
+    logger.debug(" Successfully created failed directory")
+    logger.debug("")
+else:
+    logger.debug(f" Failed Directory:")
+    logger.debug(f" - {failed_dir}")
+    logger.debug("")
 
-# Create backup directory if backup is enabled and it doesn't exist
-if backup_enabled and not os.path.exists(backup_dir):
-    os.makedirs(backup_dir)
+## Backup Directory ##
+if backup_enabled:
+    if not os.path.exists(backup_dir):
+        os.makedirs(backup_dir)
+        logger.debug(f" Backup Enabled: {backup_enabled}")
+        logger.debug(" Backup directory not found...")
+        logger.debug(" Successfully created backup directory")
+        logger.debug(f" Backup Directory:")
+        logger.debug(f" - {backup_dir}")
+    else:
+        logger.debug(f" Backup Enabled: {backup_enabled}")
+        logger.debug(f" Backup Directory:")
+        logger.debug(f" - {backup_dir}")
+else:
+    logger.debug(f" Backup Enabled: {backup_enabled}")
+    
+logger.separator(text="Processing Images", debug=False, border=True)
 
-# Match filename with directory names and copy the file
+## Match and copy Assets ##
 def match_and_copy(filename, process_dir, shows_dir, movies_dir, collections_dir, failed_dir, copied_files):
-    # Extract name and year from filename
     name, ext = os.path.splitext(filename)
     parts = name.split('(')
     if len(parts) >= 2:
-        # If filename contains parentheses, search in shows and movies directories
         title = parts[0].strip()
         year = parts[1].split(')')[0].strip()
         for directory in [shows_dir, movies_dir]:
             for dir_name in os.listdir(directory):
                 if f"{title} ({year})" in dir_name:
-                    # Copy the file to the matching directory
                     src = os.path.join(process_dir, filename)
                     dest = os.path.join(directory, dir_name, filename)
                     
-                    # Copy the file to the matching directory
                     shutil.copy(src, dest)
+                    logger.info(f" Moved {filename} to directory: {dir_name}")
                     
-                    # Rename the file based on dimensions
                     with PIL.Image.open(dest) as img:
                         width, height = img.size
                         new_name = "poster" + ext if height > width else "background" + ext
                         new_dest = os.path.join(directory, dir_name, new_name)
                         os.rename(dest, new_dest)
+                        logger.info(f" Renamed {filename} to {new_name}")
                         copied_files.append(new_dest)
                     
                     return directory
     else:
-        # If filename doesn't contain parentheses, search in collections directory
         if naming_convention == "kometa" or naming_convention == "kodi":
             for dir_name in os.listdir(collections_dir):
                 if ("Collection" in name and name.replace("Collection", "").strip() in dir_name) or name in dir_name:
                     src = os.path.join(process_dir, filename)
                     dest = os.path.join(collections_dir, dir_name, filename)
                     
-                    # Copy the file to the matching directory
                     shutil.copy(src, dest)
+                    logger.info(f" Moved {filename} to directory: {dir_name}")
                     
-                    # Rename the file based on dimensions
                     with PIL.Image.open(dest) as img:
                         width, height = img.size
                         new_name = "poster" + ext if height > width else "background" + ext
                         new_dest = os.path.join(collections_dir, dir_name, new_name)
                         os.rename(dest, new_dest)
+                        logger.info(f' Renamed {filename} to {new_name}')
                         copied_files.append(new_dest)
                     
                     return collections_dir
         else:
             move_to_failed(filename, process_dir, failed_dir)
+            logger.info(f" Failed to match {filename}")
             return failed_dir
 
-    # If no matching directory is found, move to failed directory
     move_to_failed(filename, process_dir, failed_dir)
     return None
     
-# Function to move the file to the failed directory
+## Move failed assets ##
 def move_to_failed(filename, process_dir, failed_dir):
     src = os.path.join(process_dir, filename)
     dest = os.path.join(failed_dir, filename)
     shutil.copy(src, dest)
+    logger.info(f" Failed to match {filename}. Moved to failed directory")
 
-# List to store successfully copied files
 copied_files = []
 
-# Counters for images moved to different directories
 moved_counts = {'shows_dir': 0, 'movies_dir': 0, 'collections_dir': 0, 'failed_dir': 0}
-
-# Record start time
-start_time = time.time()
 
 directory_names = {
     shows_dir: 'shows_dir',
@@ -106,52 +168,55 @@ directory_names = {
     collections_dir: 'collections_dir',
     failed_dir: 'failed_dir'
 }
-            # Match and copy files
+
+## Backup assets
 for filename in os.listdir(process_dir):
-    # Backup the file before any modification
     if backup_enabled:
         src = os.path.join(process_dir, filename)
         shutil.copy(src, os.path.join(backup_dir, filename))
+        logger.info(f" Backed up {filename} to backup directory")
 
-    # Match and copy the file
     destination = match_and_copy(filename, process_dir, shows_dir, movies_dir, collections_dir, failed_dir, copied_files)
     if destination:
         moved_counts[directory_names[destination]] += 1
 
-    # Remove the file from process_dir
     file_path = os.path.join(process_dir, filename)
     os.remove(file_path)
+    logger.info(f" Removed {filename} from process directory")
+    logger.info("")
 
-# Record end time
+## End ##
 end_time = time.time()
+logger.separator(text="Summary", debug=False, border=True)
 
-# Calculate total runtime
 total_runtime = end_time - start_time
+logger.info(f' Movie Assets: {moved_counts["movies_dir"]}')
+logger.info(f' Show Assets: {moved_counts["shows_dir"]}')
+logger.info(f' Collection Assets: {moved_counts["collections_dir"]}')
+logger.info(f' Failures: {moved_counts["failed_dir"]}')
 
-# Create a formatted summary
+## Discord Summary ##
 summary = f"**Movie Assets:**\n {moved_counts['movies_dir']}\n"
-summary += f"**TV Show Assets:**\n {moved_counts['shows_dir']}\n"
+summary += f"**Show Assets:**\n {moved_counts['shows_dir']}\n"
 summary += f"**Collection Assets:**\n {moved_counts['collections_dir']}\n"
 summary += f"**Failures:**\n {moved_counts['failed_dir']}\n"
 summary += f"**Backup Enabled?**\n {'Yes' if backup_enabled else 'No'}\n"
 summary += f"**Total Run Time:**\n {total_runtime:.2f} seconds\n"
 
-# Get the current date
 current_date = datetime.now()
 
-# Read the script version from version.txt
+## Version ##
 version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
 with open(version_file, 'r') as f:
     version = f.read().strip()
     
-# Send summary message to Discord webhook as an embed
+## Discord embed ##
 webhook_url = config.get('webhook_url')
 image_url = "https://raw.githubusercontent.com/mikenobbs/AssetAssistant/main/logo/icon.png"
-footer_text = f"AssetAssistant [v{version}] | {current_date.strftime('%d/%m/%Y %H:%M')}"
+footer_text = f"Asset Assistant [v{version}] | {current_date.strftime('%d/%m/%Y %H:%M')}"
 color = 0x9E9E9E
 
 if webhook_url:
-    # Construct the embed object with custom values
     embed = {
         "title": "Asset Assistant",
         "description": summary,
@@ -159,6 +224,7 @@ if webhook_url:
         "footer": {"text": footer_text},
         "color": color
     }
-    
-    # Send the embed data to the Discord webhook using a POST request
+
     response = requests.post(webhook_url, json={"embeds": [embed]})
+
+logger.separator(text=f'Asset Assistant Finished\nTotal runtime {total_runtime:2f} seconds', debug=False, border=True)
